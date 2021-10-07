5fbe0e9ddad29a33d6a8a1ea88b4a793fa9412dc
Offliner <40673034h@ntnu.edu.tw>
2021-10-07 20:35:21 +0800

    Update main.py

--- Exp1/main.py
+++ Exp1/main.py
@@ -50,40 +50,47 @@ ev3 = EV3Brick()
 cSensor_reflect = ColorSensor(Port.S2)
 Lmotor = Motor(Port.C)
 Rmotor = Motor(Port.B)
-mobile_car = DriveBase(Lmotor, Rmotor, wheel_diameter=55.5, axle_track=104)
+mobile_car = DriveBase(Lmotor, Rmotor, wheel_diameter=55, axle_track=120)
 
 
 # Hyperparameter
-threshold = 15
-proportion = 2
-integral = 0.000025
-derivative = 100
+black = 6
+white = 30
+threshold = (black + white) / 2
+proportion = 8
+integral = 0.001
+derivative = 25
 dt = 10
-motor_power = 100
+motor_speed = 50
 err = 0
 previous_err = 0
 sum_err = 0
 
 
 # Choose controller
-ctrl = P_controller(proportion)
+# ctrl = P_controller(proportion)
 # ctrl = PD_controller(proportion, derivative, dt)
 # ctrl = PI_controller(proportion, integral, dt)
-# ctrl = PID_controller(proportion, integral, derivative, dt)
+ctrl = PID_controller(proportion, integral, derivative, dt)
 
 
 # Start alert
 ev3.speaker.beep()
 
-while cSensor_reflect.color() != Color.RED:
+while True:
+    if cSensor_reflect.color() == Color.RED:
+        Lmotor.stop()
+        Rmotor.stop()
+        break
+
     err = threshold - cSensor_reflect.reflection()
-    gain = ctrl.control(err)
+    # gain = ctrl.control(err)
     # gain = ctrl.control(err, previous_err)
     # gain = ctrl.control(err, sum_err)
-    # gain = ctrl.control(err, sum_err, previous_err)
+    gain = ctrl.control(err, sum_err, previous_err)
     previous_err = err
     sum_err += err
-    mobile_car.drive(motor_power, gain)
+    mobile_car.drive(motor_speed, gain)
     wait(dt)
 
 
