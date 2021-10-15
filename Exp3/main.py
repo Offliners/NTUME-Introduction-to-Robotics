9ef438265c4c41bc73eca6b7e321c95e5f0fa49d
Offliner <40673034h@ntnu.edu.tw>
2021-10-15 23:35:13 +0800

    Update main.py

--- Exp3/main.py
+++ Exp3/main.py
@@ -9,64 +9,64 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 
 def Check(color, err):
     Forward(color)
-
-    if cSensor.color() == color:
-        Back(color)
-    else:
-        err += 1
-        Gripper.run_target(100, 90)
-        Back(color)
-        Gripper.run_target(-100, 90)
+    wait(1000)
+    Back(color)
+    wait(1000)
     
     return err
 
 def Forward(color):
-    mobile_car.turn(100, 180)
-    dis = 150 + Goal[color] * 100
+    dis = 210 + Goals.index(color) * 200
     mobile_car.straight(dis)
-    mobile_car.turn(100, 90)
+    Turn(-90)
     mobile_car.straight(100)
 
 def Back(color):
-    mobile_car.turn(100, 180)
-    mobile_car.straight(100)
-    mobile_car.turn(-100, 90)
-    dis = 150 + Goal[color] * 100
-    mobile_car.straight(dis)
+    mobile_car.straight(-100)
+    Turn(90)
+    dis = 210 + Goals.index(color) * 200
+    mobile_car.straight(-dis)
 
-def Sort(color, err):
-    Gripper.run_target(100, 90)
-    Forward(color)
-    Gripper.run_target(-100, 90)
-    Back(color)
-    err -= 1
+def Turn(degree):
+    curDegree = gSensor.angle()
+    if degree > 0:
+        sign = 1
+    elif degree < 0:
+        sign = -1
+    else:
+        sign = 0
 
-    return err
+    while True: 
+        print(gSensor.angle())
+        if abs(gSensor.angle() - curDegree) >= abs(degree):
+            mobile_car.stop()
+            break
 
+        mobile_car.drive(0, 100 * sign)
+        wait(5)
 
 # Initialize
 ev3 = EV3Brick()
 err = 0
 Lmotor = Motor(Port.C)
 Rmotor = Motor(Port.B)
-Gripper = Motor(Port.D)
-cSensor = ColorSensor(Port.S2)
-Goals = {Color.RED:0, Color.BLACK:1, Color.BLUE:2}
-mobile_car = DriveBase(Lmotor, Rmotor, wheel_diameter=55.5, axle_track=104)
+gSensor = GyroSensor(Port.S1)
+gSensor.reset_angle(0)
+# cSensor = ColorSensor(Port.S2)
+Goals = [Color.RED, Color.BLACK, Color.BLUE]
+mobile_car = DriveBase(Lmotor, Rmotor, wheel_diameter=55.5, axle_track=120)
 
 # Start alert
 ev3.speaker.beep()
 
 # Check the colors in order
-for c in Goals.items():
-    err = Check(c[0], err)
+for c in Goals:
+    err = Check(c, err)
+    # print(Goals.index(c))
 
-while err != 0:
-    # Todo : find ball
+# while err != 0:
+#     # Todo : find ball
     
-    err = Sort(color, err)
+#     err = Sort(color, err)
 
-
-
-# Write your program here.
 ev3.speaker.beep()
