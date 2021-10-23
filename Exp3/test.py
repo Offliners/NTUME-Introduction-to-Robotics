940783d66c89d1c7f8d31b5b3541c6af7b3ab8d5
Offliner <40673034h@ntnu.edu.tw>
2021-10-23 23:49:12 +0800

    Create test.py

---
+++ Exp3/test.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+class P_controller:
+    def __init__(self, P):
+        self.P = P
+
+    def control(self, error):
+        return self.P * error
+
+def Check(color, dis, blackExist, redExist, index):
+    Turn(-90)
+    wait(100)
+    forward = 120 - index * 10
+
+    mobile_car.straight(forward)
+
+    flag = 0
+    print(cSensor.color())
+    if (cSensor.color() == Color.BLACK or cSensor.color() == None) and blackExist == 1:
+        gripperMotor.run_angle(-50, 30)
+        blackExist = 0
+        flag = 1
+    elif cSensor.color() == Color.RED and blackExist == 0 and redExist == 1:
+        gripperMotor.run_angle(-50, 30)
+        redExist = 0
+        flag = 2
+    elif cSensor.color() == Color.BLUE and blackExist == 0 and redExist == 0:
+        gripperMotor.run_angle(-50, 30)
+        flag = 3
+
+    mobile_car.straight(-forward)
+    Turn(0)
+    wait(100)
+    
+    if flag == 1:
+        print('Throw black')
+        mobile_car.straight(660 - dis)
+        gripperMotor.run_angle(50, 30)
+        Turn(0)
+        wait(100)
+        blackExist = 0
+    elif flag == 2:
+        print('Correct red')
+        print(gSensor.angle())
+        mobile_car.straight(240 - dis)
+        Turn(90)
+        mobile_car.straight(forward)
+        gripperMotor.run_angle(50, 30)
+        wait(100)
+        mobile_car.straight(-forward)
+        Turn(0)
+        redExist = 0
+
+    return blackExist, redExist
+
+
+def Turn(degree):
+    if degree >= 0:
+        sign = 1
+    else:
+        sign = -1
+
+    while True:
+        if sign * gSensor.angle() <=  degree:
+            ctrl = P_controller(0.1)
+            gain = 10
+            while sign * gSensor.angle() !=  degree:
+                gain += ctrl.control(gSensor.angle() - sign * degree)
+                mobile_car.drive(0, gain)
+                wait(5)
+            mobile_car.stop()
+            break
+
+        mobile_car.drive(0, 50 * sign)
+        wait(10)
+
+# Initialize
+ev3 = EV3Brick()
+err = 0
+Lmotor = Motor(Port.C)
+Rmotor = Motor(Port.B)
+gripperMotor = Motor(Port.D)
+gripperMotor.reset_angle(0)
+gSensor = GyroSensor(Port.S1)
+gSensor.reset_angle(0)
+cSensor = ColorSensor(Port.S2)
+Goals = [[Color.RED, 240], [Color.BLACK, 210], [Color.BLUE, 210]]
+mobile_car = DriveBase(Lmotor, Rmotor, wheel_diameter=55.5, axle_track=120)
+mobile_car.reset()
+
+# Start alert
+ev3.speaker.beep()
+
+dis = 0
+blackExist = 1
+redExist = 1
+
+print('Round 1')
+for c in Goals:
+    mobile_car.straight(c[1])
+    dis += c[1]
+    blackExist, redExist = Check(c[0], dis, blackExist, redExist, 0)
+
+    if blackExist == 0:
+        break
+        
+mobile_car.straight(-660)
+Turn(0)
+
+dis = 20
+
+print('Round 2')
+
+for c in Goals:
+    mobile_car.straight(c[1])
+    dis += c[1]
+    blackExist, redExist = Check(c[0], dis, blackExist, redExist, 2)
+
+    if redExist == 0:
+        break
+
+mobile_car.straight(-240)
+Turn(0)
+
+dis = 20
+
+print('Round 3')
+
+for c in Goals:
+    mobile_car.straight(c[1])
+    dis += c[1]
+    blackExist, redExist = Check(c[0], dis, blackExist, redExist, 2)
+
+mobile_car.straight(-660)
+
+ev3.speaker.beep()
