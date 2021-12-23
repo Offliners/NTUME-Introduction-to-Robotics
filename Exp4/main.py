b4549e62356d7c31975a763aa8ffcd142e06d54a
Offliners <eric21904@gmail.com>
2021-12-23 14:58:21 +0800

    Exp 4

--- Exp4/main.py
+++ Exp4/main.py
@@ -1,133 +1,177 @@
-#!/usr/bin/env pybricks-micropython
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
+#!/usr/bin/env python3
 
-PI = 3.141592653589793238462643383279
-X_diameter = 43.2 # mm
-y_diameter = 56 # mm
+from time import sleep
+from ev3dev2 import list_devices
+from ev3dev2.port import LegoPort
+from ev3dev2.sound import Sound
+from ev3dev2.motor import OUTPUT_B, OUTPUT_C, OUTPUT_D, LargeMotor, MediumMotor
+from threading import Thread
+import os
+import numpy as np
 
-def liftPen(select):
-    if select:
-        pen_motor.run_angle(50, 30)
-    else:
-        pen_motor.run_angle(50, -30)
+# Large Motor Build Class 
+class MotorControl(Thread):
+    def __init__(self, port, rot_dir):
+        Thread.__init__(self)
+        self.m = LargeMotor(port)
+        self.m.polarity = rot_dir
 
-def horizonMove(dx):
-    rotation = dx / (PI * X_diameter / 10)
-    horizontal_motor.run_angle(-100, 360 * rotation)
-    horizontal_motor.stop()
-
-def verticalMove(dy):
-    rotation = dy / (PI * y_diameter / 10)
-    vertical_motor.run_angle(100, 360 * rotation)
-    vertical_motor.stop()
-
-# Draw Sun
-def drawSun(size=2):
-    liftPen(False)
-    horizonMove(2*size)
-    verticalMove(3*size)
-    liftPen(True)
-    verticalMove(-3*size)
-    horizonMove(-2*size)
-
-    liftPen(False)
-    verticalMove(3*size)
-
-    liftPen(True)
-    verticalMove(-1.5*size)
-
-    liftPen(False)
-    horizonMove(2*size)
-
-    liftPen(True)
-    horizonMove(-2*size)
-    verticalMove(1.5*size)
-
-    liftPen(False)
-    horizonMove(2*size)
-
-    liftPen(True)
-
-# Draw Spring
-def drawSpring():
-    horizonMove(1)
-    liftPen(False)
-    horizonMove(3)
-
-    liftPen(True)
-    horizonMove(-2.5)
-    verticalMove(0.75)
-
-    liftPen(False)
-    horizonMove(2)
-
-    liftPen(True)
-    horizonMove(-3.5)
-    verticalMove(0.75)
-
-    liftPen(False)
-    horizonMove(5)
-
-    liftPen(True)
-    horizonMove(-2.5)
-    verticalMove(-1.5)
-
-    liftPen(False)
+    def run(self, speed, theta):
+        self.m.on_for_degrees(speed, theta) # brake=False
     
-    horizontal_motor.run_angle(-75, 360 * (-3) / (PI * X_diameter / 10), Stop.COAST, False)
-    vertical_motor.run_angle(100, 360 * 4 / (PI * y_diameter / 10), Stop.COAST, False)
-    wait(1400)
+    def motorInfo(self):
+        print(self.m.state)
     
-    vertical_motor.stop()
-    horizontal_motor.stop()
+    def reset(self):
+        self.m.reset()
     
-    liftPen(True)
-    horizonMove(1)
-    verticalMove(-3.5)
+    def stop(self):
+        self.m.stop()
 
-    while touch_sensor.pressed() != True:
-        horizontal_motor.run(150)
+# Two Link Manipulator Class
+class TwoLinkManipulator:
+    def __init__(self, l1, l2, decelerate_ratio):
+        self.L1 = l1
+        self.L2 = l2
+        self.dr = decelerate_ratio
+        self.J0_loc = np.array([0, 0])
     
-    horizonMove(2)
-    liftPen(False)
-    horizontal_motor.run_angle(-75, 360 * 2 / (PI * X_diameter / 10), Stop.COAST, False)
-    vertical_motor.run_angle(100, 360 * 3 / (PI * y_diameter / 10), Stop.COAST, False)
-    wait(600)
+    def FK(self, theta1, theta2):
+        J1_loc = self.J0_loc + [self.L1 * np.cos(theta1), self.L1 * np.sin(theta1)]
+        J2_loc = J1_loc + [self.L2 * np.cos(theta1 + theta2), self.L2 * np.sin(theta1 + theta2)]
+        return J1_loc, J2_loc
+
+    def IK(self, x, y):
+        theta2 = np.arccos((x ** 2 + y ** 2 - self.L1 ** 2 - self.L2 ** 2) / (2 * self.L1 * self.L2))
+        theta1 = np.arctan2(y, x) - np.arctan2(self.L2 * np.sin(theta2), self.L2 * np.cos(theta2) + self.L1)
+        return self.dr * np.degrees(theta1), self.dr * np.degrees(theta2)
+
+    def move(self, theta1, theta2):
+        t12 = abs(theta1) + abs(theta2)
+        maxspeed = 30
+        if theta1 != 0 or theta2 != 0:
+            p1 = maxspeed * abs(theta1) / t12
+            p2 = maxspeed * abs(theta2) / t12
+            t1 = Thread(target=m1.run, args=(p1, theta1,))
+            t2 = Thread(target=m2.run, args=(p2, theta2,))
+            t1.start()
+            t2.start()
+            t1.join()
+            t2.join()
     
-    liftPen(True)
-    horizonMove(-3)
-
-    drawSun(1.2)
-
-
-
-
 # Initialize
-ev3 = EV3Brick()
-pen_motor = Motor(Port.A)
-horizontal_motor = Motor(Port.B)
-vertical_motor = Motor(Port.C)
-touch_sensor = TouchSensor(Port.S1)
+sound = Sound()
+m1 = MotorControl(OUTPUT_B, 'normal')
+m2 = MotorControl(OUTPUT_C, 'normal')
+m3 = MediumMotor(OUTPUT_D)
+decelerate_ratio = 24.5
+arm = TwoLinkManipulator(10, 8.5, decelerate_ratio)
 
-pen_motor.reset_angle(0)
-horizontal_motor.reset_angle(0)
-vertical_motor.reset_angle(0)
+# SUN
+# sun_dx = np.array([-4, 5.65685, -5.65685, -4.24264, 5.65685, 1.41421, -4.24264, 7.07107, -4.24264])
+# sun_dy = np.array([-1, 5.65685, -5.65685, 4.24264, 5.65685, -7.07107, 4.24264, -1.41421, 4.24264])
+# sun_z = np.array([0, 1, 0, 1, 1, 0, 1, 0, 1])
 
-# Set origin
-while touch_sensor.pressed() != True:
-    horizontal_motor.run(150)
+sun_dx = np.array([-4, 5.65685, -5.65685, -4.24264, 5.65685, 2.5, -6, 9.5, -7])
+sun_dy = np.array([-1, 5.65685, -5.65685, 4.24264, 5.9, -8, 6, -4.5, 7])
+sun_z = np.array([0, 1, 0, 1, 1, 0, 1, 0, 1])
 
-horizontal_motor.stop()
-wait(100)
+# Spring
+spring_dx = np.array([-4, -3.5355, 4.5, -3, 4.5, -4.8, 1, 1.5, 1.5, 2, -5, 0, 3.5, 2.7, -2.7, -2.5, 3.2, 0.7071, -2.8, 4.5, -3.5])
+spring_dy = np.array([-1, 3.5355, -2.8, 3, -3.5, 5, -3.5, 0, -0.5, -1, 4.5, 3, -5.2, 2.7, -2.7, 2.1213, 2.8284, -4, 2.8, -2.5, 3.5])
+spring_z = np.array([0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1])
 
-# drawSun()
-drawSpring()
+start_point = np.array([9.3, 5.2])
+def TrajectoryPlanner(dx, dy, z, start):
+    t_x = []
+    t_y = []
+    t_z = []
+    t1 = start[0]
+    t2 = start[1]
+    interval = 20
+    for i in range(dx.shape[0]):
+        for j in range(interval):
+            t1 += dx[i] * 1 / interval
+            t2 += dy[i] * 1 / interval
+            temp1, temp2 = arm.IK(t1, t2)
+            t_x.append(temp1)
+            t_y.append(temp2)
+            t_z.append(z[i])
 
-# End 
-ev3.speaker.beep()
+    return t_x, t_y, t_z
+
+def Menu():
+    history_angle = np.array([0, 0])
+    while True:
+        os.system('cls || clear')
+        print('====== Two Link Manipulator ======')
+        print('1. Move to origin')
+        print('2. Parameeter setting')
+        print('3. Hand control mode')
+        print('4. Write SUN')
+        print('5. Write Spring')
+        print('6. Motor Info')
+        print('0. Exit')
+        print('==================================')
+        
+        select = int(input('Please input your select : '))
+        global arm
+        if select == 0:
+            break
+        elif select == 1:
+            arm.move(-history_angle[0], -history_angle[1])
+        elif select == 2:
+            l1 = float(input('Please input length of Link 1 : '))
+            l2 = float(input('Please input length of Link 2 : '))
+            arm = TwoLinkManipulator(l1, l2, None)
+        elif select == 3:
+            while True:
+                t1, t2, t3 = map(float, input('Please input theta1, theta2 and theta3 (split by space, input \'0 0 0\' to leave this mode) : ').split(' '))
+                if t1 == 0 and t2 == 0 and t3 == 0:
+                    break
+                else:
+                    m3.on_for_degrees(30, t3)
+                    arm.move(decelerate_ratio * t1, decelerate_ratio * t2)
+                    endpoint = start_point + np.array([-3, 6])
+        elif select == 4:
+            t_x, t_y, t_z = TrajectoryPlanner(sun_dx, sun_dy, sun_z, start_point)
+            for i in range(1, len(t_x)):
+                m3.on_for_degrees(30, 90 * (t_z[i] - t_z[i - 1]))
+                arm.move((t_x[i] - t_x[i - 1]), (t_y[i] - t_y[i - 1]))
+            m3.on_for_degrees(30, -90)
+            
+            history_angle = np.array([t_x[-1] - t_x[0], t_y[-1] - t_y[0]])
+            arm.move(-history_angle[0], -history_angle[1])
+        elif select == 5:
+            t_x, t_y, t_z = TrajectoryPlanner(spring_dx, spring_dy, spring_z, start_point)
+            for i in range(1, len(t_x)):
+                m3.on_for_degrees(30, 90 * (t_z[i] - t_z[i - 1]))
+                arm.move((t_x[i] - t_x[i - 1]), (t_y[i] - t_y[i - 1]))
+            m3.on_for_degrees(30, -90)
+            
+            history_angle = np.array([t_x[-1] - t_x[0], t_y[-1] - t_y[0]])
+            arm.move(-history_angle[0], -history_angle[1])
+        elif select == 6:
+            print('Motor 1 info : ')
+            m1.motorInfo()
+            print('Motor 2 info : ')
+            m2.motorInfo()
+            print('Motor 3 info : ')
+            print(m3.state)
+            input('Press enter to exit...')
+    return 0
+
+# Start Alert
+sound.beep()
+
+if Menu() == 0:
+    print('End')
+else:
+    print('Error')
+
+m1.reset()
+m2.reset()
+m3.reset()
+
+# End Alert
+sound.beep()
---
+++ Exp4/write_sun/main.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+
+from time import sleep
+from ev3dev2 import list_devices
+from ev3dev2.port import LegoPort
+from ev3dev2.sound import Sound
+from ev3dev2.motor import OUTPUT_B, OUTPUT_C, OUTPUT_D, LargeMotor, MediumMotor
+from threading import Thread
+import os
+import numpy as np
+
+# Large Motor Build Class 
+class MotorControl(Thread):
+    def __init__(self, port, rot_dir):
+        Thread.__init__(self)
+        self.m = LargeMotor(port)
+        self.m.polarity = rot_dir
+
+    def run(self, speed, theta):
+        self.m.on_for_degrees(speed, theta) # brake=False
+    
+    def motorInfo(self):
+        print(self.m.state)
+    
+    def reset(self):
+        self.m.reset()
+    
+    def stop(self):
+        self.m.stop()
+
+# Two Link Manipulator Class
+class TwoLinkManipulator:
+    def __init__(self, l1, l2, decelerate_ratio):
+        self.L1 = l1
+        self.L2 = l2
+        self.dr = decelerate_ratio
+        self.J0_loc = np.array([0, 0])
+    
+    def FK(self, theta1, theta2):
+        J1_loc = self.J0_loc + [self.L1 * np.cos(theta1), self.L1 * np.sin(theta1)]
+        J2_loc = J1_loc + [self.L2 * np.cos(theta1 + theta2), self.L2 * np.sin(theta1 + theta2)]
+        return J1_loc, J2_loc
+
+    def IK(self, x, y):
+        theta2 = np.arccos((x ** 2 + y ** 2 - self.L1 ** 2 - self.L2 ** 2) / (2 * self.L1 * self.L2))
+        theta1 = np.arctan2(y, x) - np.arctan2(self.L2 * np.sin(theta2), self.L2 * np.cos(theta2) + self.L1)
+        return self.dr * np.degrees(theta1), self.dr * np.degrees(theta2)
+
+    def move(self, theta1, theta2):
+        t12 = abs(theta1) + abs(theta2)
+        maxspeed = 30
+        if theta1 != 0 or theta2 != 0:
+            p1 = maxspeed * abs(theta1) / t12
+            p2 = maxspeed * abs(theta2) / t12
+            t1 = Thread(target=m1.run, args=(p1, theta1,))
+            t2 = Thread(target=m2.run, args=(p2, theta2,))
+            t1.start()
+            t2.start()
+            t1.join()
+            t2.join()
+    
+# Initialize
+sound = Sound()
+m1 = MotorControl(OUTPUT_B, 'normal')
+m2 = MotorControl(OUTPUT_C, 'normal')
+m3 = MediumMotor(OUTPUT_D)
+decelerate_ratio = 24.5
+arm = TwoLinkManipulator(10, 8.5, decelerate_ratio)
+
+# SUN
+sun_dx = np.array([-4, 5.65685, -5.65685, -4.24264, 5.65685, 2.5, -6, 9.5, -7])
+sun_dy = np.array([-1, 5.65685, -5.65685, 4.24264, 5.9, -8, 6, -4.5, 7])
+sun_z = np.array([0, 1, 0, 1, 1, 0, 1, 0, 1])
+start_point = np.array([9.3, 5.2])
+def TrajectoryPlanner(dx, dy, z, start):
+    t_x = []
+    t_y = []
+    t_z = []
+    t1 = start[0]
+    t2 = start[1]
+    interval = 20
+    for i in range(dx.shape[0]):
+        for j in range(interval):
+            t1 += dx[i] * 1 / interval
+            t2 += dy[i] * 1 / interval
+            temp1, temp2 = arm.IK(t1, t2)
+            t_x.append(temp1)
+            t_y.append(temp2)
+            t_z.append(z[i])
+
+    return t_x, t_y, t_z
+
+# Start Alert
+sound.beep()
+
+history_angle = np.array([0, 0])
+t_x, t_y, t_z = TrajectoryPlanner(sun_dx, sun_dy, sun_z, start_point)
+for i in range(1, len(t_x)):
+    m3.on_for_degrees(30, 90 * (t_z[i] - t_z[i - 1]))
+    arm.move((t_x[i] - t_x[i - 1]), (t_y[i] - t_y[i - 1]))
+m3.on_for_degrees(30, -90)
+
+history_angle = np.array([t_x[-1] - t_x[0], t_y[-1] - t_y[0]])
+arm.move(-history_angle[0], -history_angle[1])
+
+m1.reset()
+m2.reset()
+m3.reset()
+
+# End Alert
+sound.beep()
